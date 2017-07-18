d05f349dbb54ac2205494b15d1837745102b98dc
ChengxingHuang <chengxinghuang@126.com>
2017-07-18 15:51:36 +0800

    通过Fragment+ViewPager+FragmentPagerAdapter实现左右滑动

--- app/src/main/java/com/file/filemanager/MainActivity.java
+++ app/src/main/java/com/file/filemanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.file.filemanager;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager_main);
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_main);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/file/filemanager/MyFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.file.filemanager;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public static final int PAGE_FRAGMENT_COUNT = 2;
+
+    private int[] mTabTitles = {R.string.category_storage, R.string.phone_storage};
+    private Context mContext;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new CategoryFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_FRAGMENT_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(mTabTitles[position]);
+    }
+}
