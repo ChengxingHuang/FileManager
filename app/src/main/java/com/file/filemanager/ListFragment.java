4985d5090b107fa950e02bdf61562af1a771eaeb
ChengxingHuang <chengxinghuang@126.com>
2017-07-24 21:49:56 +0800

    1、动态申请权限 2、获取文件信息（需要再优化）

--- app/src/main/java/com/file/filemanager/ListFragment.java
+++ app/src/main/java/com/file/filemanager/ListFragment.java
@@ -1,15 +1,16 @@
 package com.file.filemanager;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +18,16 @@ import java.util.Map;
 
 public class ListFragment extends Fragment {
 
-    private List<Map<String, Object>> mDataList;
+    private List<Map<String, Object>> mDataList = new ArrayList<Map<String, Object>>();
+    private List<Map<String, Object>> mFileList = new ArrayList<Map<String, Object>>();
 
     private RecyclerView mRecyclerView;
     private RecyclerViewAdapter mAdapter;
+
+    private String mAbsolutePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+    private int mFoldersCount = 0;
+    private int mFilesCount = 0;
+    private String mFileName[];
 
     public ListFragment() {
     }
@@ -42,6 +49,11 @@ public class ListFragment extends Fragment {
             @Override
             public void onItemClick(View v, int position) {
                 // TODO: 2017/7/21 点击文件：打开或提示无法打开；点击文件夹：进入下一级目录
+                if(position < mFoldersCount){
+                    mAbsolutePath = mAbsolutePath + '/' + mFileName[position];
+                    initData();
+                    mAdapter.notifyDataSetChanged();
+                }
             }
 
             @Override
@@ -54,25 +66,48 @@ public class ListFragment extends Fragment {
         return v;
     }
 
-    // TODO: 2017/7/20 获取目录结构下所有文件信息 
     private void initData(){
-        mDataList = new ArrayList<Map<String, Object>>();
+        mFileName = null;
+        mDataList.clear();
+        mFileList.clear();
+        mFileName = (new File(mAbsolutePath)).list();
 
-        for(int i = 0; i < 50; i++){
+        // TODO: 2017/7/24 获取系统文件的方式需要修改 
+        for(int i = 0; i < mFileName.length; i++){
             Map<String, Object> map = new HashMap<String, Object>();
 
-            if(i % 2 == 0) {
-                map.put("FileTypeImage", R.drawable.file_type_folder);
-                map.put("FileName", "folder" + i);
-            }else{
-                map.put("FileTypeImage", R.drawable.file_type_document);
-                map.put("FileName", "document" + i);
-            }
-            map.put("LastChangeDate", "2017-7-19");
-            map.put("LastChangeTime", "10:40");
-            map.put("IncludeFileCount", "" + i);
+            File fileTemp = new File(mAbsolutePath + '/' + mFileName[i]);
+            int fileTypeImage = 0;
+            String includeFileCount = null;
 
-            mDataList.add(map);
+            if(fileTemp.isDirectory()) {
+                fileTypeImage = R.drawable.file_type_folder;
+                includeFileCount = FileUtils.getChildFolderFileCount(getActivity(), mAbsolutePath + '/' + mFileName[i]);
+            }else if(fileTemp.isFile()){
+                // TODO: 2017/7/21 根据MIME_TYPE，显示不同的图标 
+                fileTypeImage = R.drawable.file_type_document;
+                includeFileCount = FileUtils.sizeToHumanString(fileTemp.length());
+            }
+
+            map.put("FileTypeImage", fileTypeImage);
+            map.put("FileName", fileTemp.getName());
+            map.put("LastChangeDate", FileUtils.getLastChangeDate(fileTemp));
+            map.put("LastChangeTime", FileUtils.getLastChangeTime(fileTemp));
+            map.put("IncludeFileCount", includeFileCount);
+
+            if(fileTemp.isDirectory()) {
+                mDataList.add(map);
+            }else if(fileTemp.isFile()){
+                mFileList.add(map);
+            }
+        }
+
+        mFoldersCount = mDataList.size();
+        mFilesCount = mFileList.size();
+
+        // TODO: 2017/7/21 先显示文件夹再显示文件，这种方式比较渣，待优化
+        for(int i = 0; i < mFileList.size(); i++) {
+            mDataList.add(mFileList.get(i));
         }
     }
 }
