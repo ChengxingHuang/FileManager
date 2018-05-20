948c37b2e8302e65a9ec40c1dfd6ad9d33cf0276
ChengxingHuang <chengxinghuang@126.com>
2018-05-20 15:30:01 +0800

    添加MediaStoreUtils类，黏贴、删除、新建时都需要通过这个类更新MediaStore，

---
+++ app/src/main/java/com/file/filemanager/Utils/MediaStoreUtils.java
@@ -0,0 +1,47 @@
+package com.file.filemanager.Utils;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+
+import java.util.List;
+
+/**
+ * Created by huang on 2018/5/16.
+ */
+
+public class MediaStoreUtils {
+
+    public static void scanPathForMediaStore(Context context, String path) {
+        if (context != null && !TextUtils.isEmpty(path)) {
+            String[] paths = { path };
+            MediaScannerConnection.scanFile(context, paths, null, null);
+        }
+    }
+
+    public static void scanPathForMediaStore(Context context, List<String> scanPaths) {
+        int length = scanPaths.size();
+        if (context != null && length > 0) {
+            String[] paths = new String[length];
+            scanPaths.toArray(paths);
+            MediaScannerConnection.scanFile(context, paths, null, null);
+        }
+    }
+
+    public static void deleteFileInMediaStore(Context context, String path) {
+        if (TextUtils.isEmpty(path)) {
+            return;
+        }
+
+        Uri uri = MediaStore.Files.getContentUri("external");
+        String where = MediaStore.Files.FileColumns.DATA + "=?";
+        String[] whereArgs = new String[] { path };
+        if (context != null) {
+            ContentResolver cr = context.getContentResolver();
+            cr.delete(uri, where, whereArgs);
+        }
+    }
+}
